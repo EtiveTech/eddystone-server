f970df5ac8abdd20af2bd1dc760eed3c96831c2e
Phil Crooks <coder.crooks@gmail.com>
2017-05-05 11:55:00 +0100

    Updated to support testing of nested locations.

--- grails-app/init/org/etive/city4age/repository/BootStrap.groovy
+++ grails-app/init/org/etive/city4age/repository/BootStrap.groovy
@@ -3,9 +3,10 @@ package org.etive.city4age.repository
 class BootStrap {
 
     def init = { servletContext ->
-        def location = new Location(name: "Chemist", type: "chemist", latitude: "1.111", longitude: "-2.222").save(failOnError: true)
-        new Beacon(beaconId: "c4a000000001", location: location).save(failOnError: true)
-        new Beacon(beaconId: "c4a00000275c", location: location).save(failOnError: true)
+        def shoppingCentre = new Location(name: "Harlequin Shopping Centre", type: "shoppingCentre", latitude: "3.333", longitude: "-4.444").save(failOnError: true)
+        def chemist = new Location(name: "Chemist", type: "chemist",  container: shoppingCentre, latitude: "1.111", longitude: "-2.222").save(failOnError: true)
+        new Beacon(beaconId: "c4a00000275c", location: chemist).save(failOnError: true)
+        new Beacon(beaconId: "c4a00000274a", location: shoppingCentre).save(failOnError: true)
     }
     def destroy = {
     }
