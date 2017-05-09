1c86a375b1969d5617e7ed910306fd9d6188d4bf
Phil Crooks <coder.crooks@gmail.com>
2017-05-09 16:06:18 +0100

    Bug fix (typo)

--- grails-app/init/org/etive/city4age/repository/BootStrap.groovy
+++ grails-app/init/org/etive/city4age/repository/BootStrap.groovy
@@ -5,7 +5,7 @@ class BootStrap {
     def init = { servletContext ->
         def shoppingCentre = new Location(name: "Harlequin Shopping Centre", type: "shoppingCentre", latitude: "3.333", longitude: "-4.444").save(failOnError: true)
         def chemist = new Location(name: "Chemist", type: "chemist",  container: shoppingCentre, latitude: "1.111", longitude: "-2.222").save(failOnError: true)
-        new Beacon(beaconId: "c4a00000275c", location: chemist, decription: "Front door").save(failOnError: true)
+        new Beacon(beaconId: "c4a00000275c", location: chemist, description: "Front door").save(failOnError: true)
         new Beacon(beaconId: "c4a00000274a", location: shoppingCentre, description: "High St. entrance").save(failOnError: true)
     }
     def destroy = {
