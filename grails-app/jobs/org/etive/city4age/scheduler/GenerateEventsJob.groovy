65e2b3cf9201078a116b52f00ef5dfc1f952c467
Phil Crooks <coder.crooks@gmail.com>
2017-06-29 14:26:21 +0100

    Most of the code moved to PoiEventGenerator

--- grails-app/jobs/org/etive/city4age/scheduler/GenerateEventsJob.groovy
+++ grails-app/jobs/org/etive/city4age/scheduler/GenerateEventsJob.groovy
@@ -1,47 +1,16 @@
 package org.etive.city4age.scheduler
 
-import org.etive.city4age.repository.ProximityEventList
-import org.etive.city4age.repository.PoiEvent
+import org.etive.city4age.repository.PoiEventGenerator
 
 class GenerateEventsJob {
-    def proximityEventService
-    def PoiEventService
-    def careReceiverService
 
     static triggers = {
-        cron name: 'poiTrigger', cronExpression: "0 30 2 * * ?"
-//        cron name: 'poiTrigger', cronExpression: "0 26 * * * ?"
+//        cron name: 'poiTrigger', cronExpression: "0 30 2 * * ?"
+        cron name: 'poiTrigger', cronExpression: "0 11 * * * ?"
     }
 
     def execute() {
         // execute job
-        def careReceivers = careReceiverService.listCareReceivers()
-        def today = (new Date()).clearTime()
-        def yesterday = today // - 1
-        for (receiver in careReceivers) {
-            Date start = receiver.eventsGenerated
-            if (!start) {
-                def first = proximityEventService.firstProximityEvent(receiver)
-                start = (first) ? first.timestamp : yesterday
-            }
-            start.clearTime()
-            Date date = new Date(start.getTime())
-            for (; date <= yesterday; date += 1) {
-                ProximityEventList list = new ProximityEventList(proximityEventService.forProcessing(receiver, date))
-                def events = PoiEvent.findEvents(receiver, list)
-                if (events) {
-                    def timestamp = null
-                    for (event in events) {
-                        def poiEvent = poiEventService.createPoiEvent(event)
-                        if (!poiEvent) break
-                        timestamp = poiEvent.timestamp
-                    }
-                    if (timestamp) {
-                        receiver.eventsGenerated = timestamp
-                        careReceiverService.persistChanges(receiver)
-                    }
-                }
-            }
-        }
+        new PoiEventGenerator().generateEvents()
     }
 }
