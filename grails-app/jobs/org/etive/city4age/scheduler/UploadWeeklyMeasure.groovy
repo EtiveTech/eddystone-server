05338969e216ab48d15a9f6a8aa067bbb4fa9b3b
EtiveAlistair <37333948+EtiveAlistair@users.noreply.github.com>
2018-06-11 16:41:25 +0100

    First commit

---
+++ grails-app/jobs/org/etive/city4age/scheduler/UploadWeeklyMeasure.groovy
@@ -0,0 +1,17 @@
+package org.etive.city4age.scheduler
+
+import org.etive.city4age.repository.CentralRepositorySession
+
+class UploadWeeklyMeasure {
+
+    def activityRecordService
+
+
+    private final String centralRepository = System.getenv("CENTRAL_ADDRESS")
+
+    static triggers = {
+        cron name: 'uploadWeeklyTrigger', cronExpression: "0 30 1 * * MON"
+    }
+
+
+}
